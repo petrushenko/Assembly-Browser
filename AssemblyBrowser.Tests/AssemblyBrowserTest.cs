58359d88ed28b0f52662e90b9336b98290acdaae
petrushenko <igor.petrushenko1@gmail.com>
2019-10-23 17:39:54 +0300

    Added unit tests project for assembly browser lib

---
+++ AssemblyBrowser.Tests/AssemblyBrowserTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AssemblyBrowserLib.Tests
+{
+    [TestClass]
+    public class AssemblyBrowserTest
+    {
+        [TestMethod]
+        public void Test()
+        {
+            var assemblyBrowser = new AssemblyBrowser();
+            var str = assemblyBrowser.GetNamespaces(@"D:\bsuir\C#\projects\Faker\Faker\bin\Debug\net472\Faker.dll");
+        }
+    }
+}
