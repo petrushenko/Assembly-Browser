a7bf65e1891097f495282dfde621c52bfdd649ae
petrushenko <igor.petrushenko1@gmail.com>
2019-10-28 15:50:32 +0300

    Added unit test project

--- AssemblyBrowser.Tests/AssemblyBrowserTest.cs
+++ AssemblyBrowser.Tests/AssemblyBrowserTest.cs
@@ -10,7 +10,7 @@ namespace AssemblyBrowserLib.Tests
         public void Test()
         {
             var assemblyBrowser = new AssemblyBrowser();
-            var str = assemblyBrowser.GetNamespaces(@"D:\bsuir\C#\projects\Faker\Faker\bin\Debug\net472\Faker.dll");
+            var str = assemblyBrowser.GetNamespaces(@"D:\bsuir\C#\projects\ConsoleApp1\ConsoleApp2\bin\Debug\ConsoleApp2.exe");
         }
     }
 }
