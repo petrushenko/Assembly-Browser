04f326541bcfbfc9404b7481ceee80823f15b69a
petrushenko <igor.petrushenko1@gmail.com>
2019-10-29 19:53:21 +0300

    Code clenup

--- AssemblyBrowserLib/IAssemblyBrowser.cs
+++ AssemblyBrowserLib/IAssemblyBrowser.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace AssemblyBrowserLib
+﻿namespace AssemblyBrowserLib
 {
-    interface IAssemblyBrowser
+    public interface IAssemblyBrowser
     {
         ContainerInfo[] GetNamespaces(string assemblyPath);
     }
--- AssemblyBrowserLib/NameSpaceInfo.cs
+++ AssemblyBrowserLib/NameSpaceInfo.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace AssemblyBrowserLib
+﻿namespace AssemblyBrowserLib
 {
     public class NamespaceInfo : ContainerInfo
     {
